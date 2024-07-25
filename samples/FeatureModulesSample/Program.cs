e92c5bd1efe4bab12d69fbacb6a0c57c1558c80c
Peter Nylander <peter.nylander@gmail.com>
2024-07-25 22:49:04 +0200

    Add Scalar.AspNetCore and update Swagger configuration
    Updated `FeatureModulesSample.csproj` to include a new package reference for `Scalar.AspNetCore` version `1.1.2`. Added the `Scalar.AspNetCore` namespace to the using directives in `Program.cs`. Modified the Swagger configuration in `Program.cs` to define a Swagger document named "openapi", set a custom route template for Swagger JSON/YAML files, and specify an endpoint for the Swagger UI to use the "openapi" document. Added `app.MapScalarApiReference()` to the middleware pipeline configuration in `Program.cs`.

--- samples/FeatureModulesSample/Program.cs
+++ samples/FeatureModulesSample/Program.cs
@@ -1,4 +1,5 @@
 using Infinity.Toolkit.FeatureModules;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddFeatureModules();
@@ -6,19 +7,22 @@ builder.AddFeatureModules();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("openapi", new Microsoft.OpenApi.Models.OpenApiInfo ());
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options => options.RouteTemplate = "/{documentName}/v1.{json|yaml}");
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
 }
 
 app.UseHttpsRedirection();
 
 app.MapFeatureModules();
-
+app.MapScalarApiReference();
 app.Run();
