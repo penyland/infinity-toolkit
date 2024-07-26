6ddc35285d043104644c0218129243764a83aeaa
Peter Nylander <peter.nylander@gmail.com>
2024-07-26 20:48:39 +0200

    Refactor Swagger setup and update API route templates
    - Simplify `SwaggerGen` configuration by removing custom `SwaggerDoc`.
    - Update Swagger `RouteTemplate` to `"/openapi/{documentName}.{json|yaml}"`.
    - Change `SwaggerEndpoint` in `SwaggerUI` to `"/openapi/v1.json"`.
    - Move `MapScalarApiReference` call inside development environment check.

--- samples/FeatureModulesSample/Program.cs
+++ samples/FeatureModulesSample/Program.cs
@@ -7,22 +7,19 @@ builder.Logging.AddCodeThemeConsoleFormatter(_ => { });
 builder.AddFeatureModules();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("openapi", new Microsoft.OpenApi.Models.OpenApiInfo());
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(options => options.RouteTemplate = "/{documentName}/v1.{json|yaml}");
+    app.UseSwagger(options => options.RouteTemplate = "/openapi/{documentName}.{json|yaml}");
     app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
 
 app.MapFeatureModules();
-app.MapScalarApiReference();
 app.Run();
