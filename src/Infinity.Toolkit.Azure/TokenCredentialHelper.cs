f5f26cf0358ef5b6ee987e041d28f75e8ac2b41a
Peter Nylander <peter.nylander@gmail.com>
2025-01-06 20:57:04 +0100

    Add Infinity.Toolkit.Azure project and TokenCredentialHelper
    - Introduced a new project "Infinity.Toolkit.Azure" in the solution.
    - Created `Infinity.Toolkit.Azure.csproj` targeting .NET 9.0 with Azure service package references.
    - Added `TokenCredentialHelper` class to manage Azure authentication using a ChainedTokenCredential.

---
+++ src/Infinity.Toolkit.Azure/TokenCredentialHelper.cs
@@ -0,0 +1,76 @@
+﻿using Azure.Core;
+using Azure.Identity;
+
+namespace Infinity.Toolkit.Azure;
+
+public static class TokenCredentialHelper
+{
+    /// <summary>
+    /// Helps creating a ChainedTokenCredential used to authenticate with Azure services.
+    /// By default, the following credential types are included:
+    /// EnvironmentCredential
+    /// ManagedIdentityCredential
+    ///
+    /// To include additional credential types, set the corresponding environment variable to "true".
+    /// INCLUDE_VISUAL_STUDIO_CREDENTIAL
+    /// INCLUDE_VISUAL_STUDIO_CODE_CREDENTIAL
+    /// INCLUDE_INTERACTIVE_BROWSER_CREDENTIAL
+    /// INCLUDE_SHARED_TOKEN_CACHE_CREDENTIAL
+    /// INCLUDE_AZURE_DEVELOPER_CLI_CREDENTIAL
+    /// INCLUDE_AZURE_POWER_SHELL_CREDENTIAL
+    /// INCLUDE_AZURE_CLI_CREDENTIAL
+    /// INCLUDE_WORKLOAD_IDENTITY_CREDENTIAL
+    /// 
+    /// </summary>
+    /// <returns>A ChainedTokenCredential with the specified credential types.</returns>
+    public static TokenCredential GetTokenCredential()
+    {
+        TokenCredential[] tokenCredentials =[];
+
+        if (Equals(Environment.GetEnvironmentVariable("INCLUDE_AZURE_CLI_CREDENTIAL"), "true"))
+        {
+            tokenCredentials = [.. tokenCredentials, new AzureCliCredential()];
+        }
+
+        if (Equals(Environment.GetEnvironmentVariable("INCLUDE_WORKLOAD_IDENTITY_CREDENTIAL"), "true"))
+        {
+            tokenCredentials = [.. tokenCredentials, new WorkloadIdentityCredential()];
+        }
+
+        if (Equals(Environment.GetEnvironmentVariable("INCLUDE_AZURE_DEVELOPER_CLI_CREDENTIAL"), "true"))
+        {
+            tokenCredentials = [.. tokenCredentials, new AzureDeveloperCliCredential()];
+        }
+
+        if (Equals(Environment.GetEnvironmentVariable("INCLUDE_SHARED_TOKEN_CACHE_CREDENTIAL"), "true"))
+        {
+            tokenCredentials = [.. tokenCredentials, new SharedTokenCacheCredential()];
+        }
+
+        if (Equals(Environment.GetEnvironmentVariable("INCLUDE_VISUAL_STUDIO_CREDENTIAL"), "true"))
+        {
+            tokenCredentials = [.. tokenCredentials, new VisualStudioCredential()];
+        }
+
+        if (Equals(Environment.GetEnvironmentVariable("INCLUDE_VISUAL_STUDIO_CODE_CREDENTIAL"), "true"))
+        {
+            tokenCredentials = [.. tokenCredentials, new VisualStudioCodeCredential()];
+        }
+
+        if (Equals(Environment.GetEnvironmentVariable("INCLUDE_AZURE_POWER_SHELL_CREDENTIAL"), "true"))
+        {
+            tokenCredentials = [.. tokenCredentials, new AzurePowerShellCredential()];
+        }
+
+        if (Equals(Environment.GetEnvironmentVariable("INCLUDE_INTERACTIVE_BROWSER_CREDENTIAL"), "true"))
+        {
+            tokenCredentials = [.. tokenCredentials, new InteractiveBrowserCredential()];
+        }
+
+        tokenCredentials = [.. tokenCredentials, new EnvironmentCredential(), new ManagedIdentityCredential()];
+
+        var tokenCredential = new ChainedTokenCredential(tokenCredentials);
+
+        return tokenCredential;
+    }
+}
