00780e3c296caa845943f9ab7d634ddfe83bed78
Peter Nylander <peter.nylander@gmail.com>
2025-01-08 15:51:09 +0100

    Created PipelineSample app

---
+++ samples/PipelineSample/ProductCreated.cs
@@ -0,0 +1,50 @@
+﻿using Infinity.Toolkit.Experimental;
+using Infinity.Toolkit.Experimental.Mediator;
+using Infinity.Toolkit.Experimental.Pipeline;
+
+namespace PipelineSample;
+
+internal record ProductCreated(int Id, string Name) : ICommand;
+
+internal record ProductCreatedResult(string NewName);
+
+internal class ProductCreatedHandler : IMediatorHandler<ProductCreated>
+{
+    private readonly IPipeline<ProductCreated, ProductCreatedResult> pipeline;
+
+    public ProductCreatedHandler(IPipeline<ProductCreated, ProductCreatedResult> pipeline)
+    {
+        this.pipeline = pipeline;
+    }
+
+    public Task<Result> HandleAsync(MediatorHandlerContext<ProductCreated> context, CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine("ProductCreatedHandler:HandleAsync");
+        Console.WriteLine($"Product created: {context.Request.Id} - {context.Request.Name}");
+
+        Console.WriteLine("Executing pipeline");
+        //var result = await pipeline.ExecuteAsync(context.Request, cancellationToken);
+        //Console.WriteLine($"Pipeline executed: {result.NewName}");
+
+        return Task.FromResult(Result.Success());
+    }
+}
+
+public static class ProductCreatedPipeline
+{
+    internal static IPipeline<ProductCreated, ProductCreatedResult> CreatePipeline(IServiceProvider services) => new Pipeline<ProductCreated, ProductCreatedResult>()
+    .AddStep<ProductCreated, string>(input =>
+    {
+        Console.WriteLine("Step 1");
+        Console.WriteLine($"Product created: {input.Id} - {input.Name}");
+        var result = input.Name + " - Modified";
+        return result;
+    })
+    .AddStep<string, ProductCreatedResult>(input =>
+    {
+        Console.WriteLine("Step 2");
+        Console.WriteLine($"Product modified: {input}");
+        return new ProductCreatedResult(input);
+    })
+    .Build();
+}
---
+++ samples/PipelineSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Infinity.Toolkit.Experimental.Mediator;
+using Infinity.Toolkit.Experimental.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+using PipelineSample;
+
+Console.WriteLine("Hello Pipeline World!");
+
+var services = new ServiceCollection();
+services.AddMediator();
+services.AddMediatorHandler<ProductCreated, ProductCreatedHandler>();
+services.AddPipeline<ProductCreated, ProductCreatedResult>(services => ProductCreatedPipeline.CreatePipeline(services));
+
+var serviceProvider = services.BuildServiceProvider();
+var mediator = serviceProvider.GetRequiredService<IMediator>();
+await mediator.SendAsync(new ProductCreated(1, "Product 1"));
+
+Console.WriteLine("Done");
