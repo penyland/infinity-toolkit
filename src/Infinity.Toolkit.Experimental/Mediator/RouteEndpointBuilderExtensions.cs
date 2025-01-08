c01c4794d437d045859005c184923cb9f8d7e4d0
Peter Nylander <peter.nylander@gmail.com>
2025-01-08 22:29:43 +0100

    Introduced `RouteEndpointBuilderExtensions` class in `RouteEndpointBuilderExtensions.cs` with a new method `MapGetQuery<TRequest, TResponse>` to facilitate GET request mapping with query parameters using the Mediator pattern for structured request handling.

---
+++ src/Infinity.Toolkit.Experimental/Mediator/RouteEndpointBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace Infinity.Toolkit.Experimental.Mediator;
+
+public static class RouteEndpointBuilderExtensions
+{
+    public static RouteHandlerBuilder MapGetQuery<TRequest, TResponse>(this IEndpointRouteBuilder builder, string path)
+        where TRequest : class, IQuery
+    {
+        return builder.MapGet(path, async ([AsParameters] TRequest request, [FromServices] IMediator mediator) =>
+        {
+            var result = await mediator.SendAsync<TRequest, TResponse>(request);
+
+            IResult response = result switch
+            {
+                Success => TypedResults.Ok<TResponse>(result.Value),
+                Failure => TypedResults.BadRequest(),
+                _ => TypedResults.BadRequest("Failed to process request.")
+            };
+
+            return response;
+        })
+        .Produces<TResponse>(statusCode: StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest);
+    }
+}
