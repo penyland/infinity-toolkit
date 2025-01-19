88332e64405d2ed59233aa014befe93f9a451a84
Peter Nylander <peter.nylander@gmail.com>
2025-01-19 22:52:58 +0100

    Enhance Azure App Configuration support and features
    Updated `ConfigurationBuilderExtensions.cs` to add new properties
    like `ApplicationName` and `UseKeyVault` in `AzureAppConfigSettings`.
    Refactored methods for improved configuration handling and
    enhanced connection string parsing. Updated project file to
    include `Microsoft.FeatureManagement` for feature management capabilities.

--- src/Infinity.Toolkit.Azure/Configuration/ConfigurationBuilderExtensions.cs
+++ src/Infinity.Toolkit.Azure/Configuration/ConfigurationBuilderExtensions.cs
@@ -1,82 +1,168 @@
 ﻿using Azure.Core;
-using Azure.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Hosting;
+using System.Data.Common;
+using System.Text.Json.Serialization;
 
 namespace Infinity.Toolkit.Azure.Configuration;
 
 // Config section specifications
-// Infinity: {
-//   Azure: {
-//     AppConfig: {
-//       Endpoint: "https://<app-config-name>.azconfig.io",
-//       GlobalKeyFilter: "Global",
-//       UseFeatureFlags: true,
-//     }
-//   }
-// }
+//"Infinity": {
+//  "Azure": {
+//    "AppConfig": {
+//      "ApplicationName": "AzureSample",
+//      "Endpoint": "https://<app-config-name>.azconfig.io",
+//      "GlobalKeyFilter": "Global",
+//      "UseFeatureFlags": true,
+//      "UseKeyVault": true
+//    }
+//  }
+//}
 
 public class AzureAppConfigSettings
 {
-    public string Endpoint { get; init; } = string.Empty;
-    public string GlobalKeyFilter { get; init; } = "Global";
-    public bool UseFeatureFlags { get; init; } = true;
+    internal const string DefaultConfigSectionName = "Infinity:Azure:AppConfig";
+
+    public string ApplicationName { get; set; }
+
+    public Uri Endpoint { get; set; }
+
+    public string GlobalKeyFilter { get; set; } = "Global";
+
+    public bool UseFeatureFlags { get; set; } = true;
+
+    public bool UseKeyVault { get; set; } = true;
+
+    [JsonIgnore]
+    public TokenCredential? TokenCredential { get; set; } = Identity.TokenCredentialHelper.GetTokenCredential();
+
+    internal void ParseConnectionString(string? connectionString)
+    {
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException($"""
+                    ConnectionString is missing.
+                    It should be provided in 'ConnectionStrings:<connectionName>'
+                    or '{DefaultConfigSectionName}:Endpoint' key.'
+                    configuration section.
+                    """);
+        }
+
+        if (Uri.TryCreate(connectionString, UriKind.Absolute, out var uri))
+        {
+            Endpoint = uri;
+        }
+        else
+        {
+            var builder = new DbConnectionStringBuilder
+            {
+                ConnectionString = connectionString
+            };
+
+            if (builder.TryGetValue("Endpoint", out var endpoint) is false)
+            {
+                throw new InvalidOperationException($"""
+                        The 'ConnectionStrings:<connectionName>' (or 'Endpoint' key in
+                        '{DefaultConfigSectionName}') is missing.
+                        """);
+            }
+
+            if (Uri.TryCreate(endpoint.ToString(), UriKind.Absolute, out uri) is false)
+            {
+                throw new InvalidOperationException($"""
+                        The 'ConnectionStrings:<connectionName>' (or 'Endpoint' key in
+                        '{DefaultConfigSectionName}') isn't a valid URI.
+                        """);
+            }
+
+            Endpoint = uri;
+        }
+    }
 }
 
 public static class ConfigurationBuilderExtensions
 {
     private const string ConfigSectionPath = "Infinity:Azure:AppConfig";
 
-    public static IHostApplicationBuilder AddAzureAppConfiguration(this IHostApplicationBuilder builder, TokenCredential? tokenCredential = default)
-    {
-        var appConfigEndpointUri = Environment.GetEnvironmentVariable("AZURE_APP_CONFIG_ENDPOINT") ?? builder.Configuration.GetValue<string>($"{ConfigSectionPath}:Endpoint") ?? throw new ArgumentException("Azure App Configuration endpoint is not configured.");
-        var globalKeyFilter = builder.Configuration.GetValue<string>($"{ConfigSectionPath}:GlobalKeyFilter", "Global");
-        var useFeatureFlags = builder.Configuration.GetValue<bool>($"{ConfigSectionPath}:UseFeatureFlags", true);
+    public static IHostApplicationBuilder ConfigureAzureAppConfiguration(this IHostApplicationBuilder builder)
+        => builder.ConfigureAzureAppConfiguration(ConfigSectionPath, null, null);
 
-        var credentials = tokenCredential ?? new DefaultAzureCredential();
+    public static IHostApplicationBuilder ConfigureAzureAppConfiguration(this IHostApplicationBuilder app, Action<AzureAppConfigSettings>? configure = null, Action<AzureAppConfigurationRefreshOptions>? refreshOptions = null)
+        => app.ConfigureAzureAppConfiguration(ConfigSectionPath, configure, refreshOptions);
+
+    public static IHostApplicationBuilder ConfigureAzureAppConfiguration(this IHostApplicationBuilder builder, string configSectionPath = ConfigSectionPath, Action<AzureAppConfigSettings>? configureSettings = null, Action<AzureAppConfigurationRefreshOptions>? refreshOptions = null)
+    {
+        ArgumentNullException.ThrowIfNull(builder, nameof(builder));
+
+        var settings = new AzureAppConfigSettings()
+        {
+            ApplicationName = builder.Environment.ApplicationName
+        };
+
+        builder.Configuration.GetSection(configSectionPath).Bind(settings);
+        configureSettings?.Invoke(settings);
 
         builder.Configuration.AddAzureAppConfiguration(options =>
         {
-            options.Connect(new Uri(appConfigEndpointUri), credentials);
+            if (builder.Configuration.GetConnectionString("AzureAppConfig") is string connectionString)
+            {
+                settings.ParseConnectionString(connectionString);
+                options.Connect(builder.Configuration.GetConnectionString("AzureAppConfig"));
+            }
+            else
+            {
+                if (Uri.TryCreate(settings.Endpoint.ToString(), UriKind.Absolute, out var endpointUri))
+                {
+                    options.Connect(endpointUri, settings.TokenCredential);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"""
+                        The 'Endpoint' key in
+                        '{configSectionPath}') isn't a valid URI.
+                        """);
+                }
+            }
 
-            if (!string.IsNullOrEmpty(globalKeyFilter))
+            if (!string.IsNullOrEmpty(settings.GlobalKeyFilter))
             {
                 // Filter by global key filter
                 options
-                    .Select("${globalKeyFilter}*")
-                    .Select("${globalKeyFilter}*", builder.Environment.EnvironmentName);
+                    .Select($"{settings.GlobalKeyFilter}*")
+                    .Select($"{settings.GlobalKeyFilter}*", builder.Environment.EnvironmentName)
+                    .TrimKeyPrefix($"{settings.GlobalKeyFilter}:");
             }
 
-            if (!string.IsNullOrEmpty(builder.Environment.ApplicationName))
+            if (!string.IsNullOrEmpty(settings.ApplicationName))
             {
                 // Filter by application name
                 options
-                    .Select($"{builder.Environment.ApplicationName}*")
-                    .Select($"{builder.Environment.ApplicationName}*", builder.Environment.EnvironmentName);
+                    .Select($"{settings.ApplicationName}*")
+                    .Select($"{settings.ApplicationName}*", builder.Environment.EnvironmentName)
+                    .TrimKeyPrefix(settings.ApplicationName + ":");
             }
 
-            // Trim key prefixes
-            if (!string.IsNullOrEmpty(globalKeyFilter))
-            {
-                options.TrimKeyPrefix($"{globalKeyFilter}:");
-            }
-
-            options.TrimKeyPrefix(builder.Environment.ApplicationName + ":");
-
-            if (useFeatureFlags)
+            if (settings.UseFeatureFlags)
             {
                 options.UseFeatureFlags(featureFlagOptions =>
                 {
+                    // Expose refresh interval and label to settings
                     featureFlagOptions
-                        .Select($"{builder.Environment.ApplicationName}*")
-                        .Select($"{builder.Environment.ApplicationName}*", builder.Environment.EnvironmentName);
+                        .Select($"{settings.ApplicationName}*")
+                        .Select($"{settings.ApplicationName}*", builder.Environment.EnvironmentName);
                 });
             }
 
-            options.ConfigureKeyVault(kv =>
+            if (settings.UseKeyVault)
             {
-                kv.SetCredential(credentials);
-            });
+                options.ConfigureKeyVault(kvOptions =>
+                {
+                    kvOptions.SetCredential(settings.TokenCredential);
+                });
+            }
+
+            options.ConfigureRefresh(refreshOptions);
         });
 
         return builder;
