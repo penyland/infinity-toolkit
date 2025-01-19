e0943b3b280cca00c01d5b159f1386d8b574b341
Peter Nylander <peter.nylander@gmail.com>
2025-01-19 22:54:56 +0100

    Add AzureSample project with configuration and endpoints
    - Introduced a new project "AzureSample" in the solution.
    - Created `AzureSample.csproj` targeting .NET 9.0 with necessary references.
    - Added `AzureSample.http` for testing the `/weatherforecast` endpoint.
    - Updated `Program.cs` to configure Azure App Configuration and set up OpenAPI.
    - Added `launchSettings.json` for HTTP/HTTPS launch profiles.
    - Created `appsettings.Development.json` and `appsettings.json` for logging and Azure settings.

---
+++ samples/AzureSample/Program.cs
@@ -0,0 +1,59 @@
+using Azure.Identity;
+using Infinity.Toolkit.Azure.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.ConfigureAzureAppConfiguration(configureSettings: options =>
+{
+    options.TokenCredential = new AzureCliCredential();
+}, refreshOptions: refreshOptions =>
+{
+    refreshOptions.SetRefreshInterval(TimeSpan.FromSeconds(15));
+    refreshOptions.Register("Sentinel", false);
+});
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/config", ([FromServices] IConfiguration configuration) =>
+{
+    return (configuration as IConfigurationRoot)?.GetDebugView();
+});
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
