a7edf3417cbe953debb5fe0046488ec04290f90d
Peter Nylander <peter.nylander@gmail.com>
2025-01-19 23:07:40 +0100

    Add EnvironmentHelper class for container checks
    Introduces a new static class `EnvironmentHelper` in the `Infinity.Toolkit.Azure` namespace. This class provides two properties: `RunningInContainer`, which checks if the application is running in a container, and `RunningInAzureContainerApps`, which verifies if the application is running in Azure Container Apps.

---
+++ src/Infinity.Toolkit.Azure/EnvironmentHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace Infinity.Toolkit.Azure;
+public static class EnvironmentHelper
+{
+    public static bool RunningInContainer => Equals(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"), "true");
+
+    public static bool RunningInAzureContainerApps => RunningInContainer && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("CONTAINER_APP_NAME"));
+}
