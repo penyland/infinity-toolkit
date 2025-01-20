4d50b372bcf3294b9f87aac2512bee0f7c9d2e2c
Peter Nylander <peter.nylander@gmail.com>
2025-01-20 15:29:54 +0100

    Refactor EnvironmentHelper class properties
    Renamed `RunningInContainer` to `IsRunningInContainer` and added a new property `IsRunningInAzureAppService`. Updated logic for Azure Container Apps detection and changed the class declaration to `partial` for extensibility.

---
+++ src/Infinity.Toolkit/EnvironmentHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Infinity.Toolkit;
+
+public static partial class EnvironmentHelper
+{
+    public static bool IsRunningInContainer => Equals(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"), "true");
+
+    public static bool IsRunningInAzureAppService => Equals(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME"), "true");
+
+    public static bool IsRunningInAzureContainerApps => IsRunningInContainer && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("CONTAINER_APP_NAME"));
+}
