f79be2ba1d44504160bd75c3459d8a14037b4cc0
Peter Nylander <peter.nylander@gmail.com>
2025-01-29 22:17:52 +0100

    Updated Github Action (#5)
    * Update GitHub Actions workflow for NuGet publishing and testing
    
    * Add workflow dispatch option for NuGet publishing in GitHub Actions
    
    * Update project file to disable packing
    
    Added <IsPackable>false</IsPackable> to SlackMessageBuilder.csproj to indicate that the project should not be packed into a NuGet package.
    
    * Remove Infinity.Toolkit.TestUtils and enhance testing
    
    This commit removes the "Infinity.Toolkit.TestUtils" project from the solution and its associated configurations. New test utility classes, including `MockMeterFactory` and custom logger implementations (`XunitLogger` and `XunitLoggerFactory`), have been added to improve logging capabilities in unit tests. The `Infinity.Toolkit.Tests` project has been updated to reference these new utilities, streamlining the project structure while enhancing testing functionalities.
    
    * Add global using for Infinity.Toolkit.Tests.TestUtils
    
    * Update GitHub Actions workflow to support .NET 8 and streamline commands
    
    * Include LICENSE file in package output
    
    Added an `<ItemGroup>` to pack the `LICENSE` file with the package by marking it as a `None` item and specifying an empty `PackagePath`.
    
    * Refactor build configuration and file inclusions
    
    Updated `Directory.Build.props` to add new properties for
    `ContainerRuntimeIdentifier` and `EnableSdkContainerSupport`.
    Introduced conditional inclusion of LICENSE and README.md files
    based on project directory, while removing previous unconditional
    references. Adjusted `.csproj` files for various modules to
    streamline packaging and improve project structure and
    maintainability.
    
    * Remove version suffix from project file
    
    Eliminated the `VersionSuffix` element specifying "alpha-2" in the project file, indicating a potential shift towards a more stable versioning strategy.

---
+++ tests/Infinity.Toolkit.Tests/TestUtils/XunitLoggerProviderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace Infinity.Toolkit.Tests.TestUtils;
+
+public static class XunitLoggerProviderExtensions
+{
+    public static ILoggingBuilder AddXunit(this ILoggingBuilder builder, ITestOutputHelper testOutputHelper)
+    {
+        builder.Services.TryAddSingleton(testOutputHelper);
+
+        builder.Services.AddSingleton<ILoggerProvider>(new XUnitLoggerProvider(testOutputHelper));
+        return builder;
+    }
+}
