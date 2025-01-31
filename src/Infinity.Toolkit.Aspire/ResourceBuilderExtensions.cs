c6c366a6e5c7e703bddc9e2a7f9677c12b5c8708
Peter Nylander <peter.nylander@gmail.com>
2025-01-31 09:13:39 +0100

    Add Infinity.Toolkit.Aspire project and extensions
    Updated the solution to include the new `Infinity.Toolkit.Aspire` project with .NET 9.0 support.
    Created the project file with necessary configurations and a package reference to `Aspire.Hosting`.
    Introduced a new static class in `ResourceBuilderExtensions.cs` that adds an extension method for `IResourceBuilder<T>`, enabling command execution to open a URL based on resource health status.

---
+++ src/Infinity.Toolkit.Aspire/ResourceBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics;
+
+namespace Infinity.Toolkit.Aspire;
+
+internal static class ResourceBuilderExtensions
+{
+    public static IResourceBuilder<T> WithScalarCommand<T>(this IResourceBuilder<T> builder)
+        where T : IResourceWithEndpoints
+    {
+        builder.WithCommand(
+            name: "scalar-docs",
+            displayName: "Scalar api reference",
+            executeCommand: context => OnOpenUrlCommandAsync(builder, context, "scalar"),
+            updateState: OnUpdateStateResource,
+            iconName: "AnimalRabbitOff",
+            iconVariant: IconVariant.Filled);
+
+        return builder;
+    }
+
+    private static async Task<ExecuteCommandResult> OnOpenUrlCommandAsync<T>(IResourceBuilder<T> builder, ExecuteCommandContext context, string url)
+        where T : IResourceWithEndpoints
+    {
+        var endpoint = builder.Resource.GetEndpoint("https").Url;
+        var theUrl = $"{endpoint}/{url}";
+        Console.WriteLine(theUrl);
+        await Task.Run(() => Process.Start(new ProcessStartInfo
+        {
+            UseShellExecute = true,
+            FileName = theUrl,
+        }));
+        return CommandResults.Success();
+    }
+
+    private static ResourceCommandState OnUpdateStateResource(UpdateCommandStateContext context)
+    {
+        return context.ResourceSnapshot.HealthStatus == HealthStatus.Healthy
+                    ? ResourceCommandState.Enabled
+                    : ResourceCommandState.Disabled;
+    }
+}
