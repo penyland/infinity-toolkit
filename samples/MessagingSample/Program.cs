7572f144bc5ad38b1e96986375cb6d800c11e3b3
Peter Nylander <peter.nylander@gmail.com>
2025-02-11 22:37:33 +0100

    Refactor messaging and update weather forecast handling
    - Added `Microsoft.AspNetCore.Mvc` namespace for MVC features.
    - Updated `AddInfinityMessaging` to configure a new "generic" channel for producers and consumers.
    - Introduced default channel producer and consumer for enhanced messaging.
    - Changed message handler mapping to use `WeatherForecast` and added a new `DefaultMessageHandler`.
    - Updated endpoints to use `WeatherForecast` for consistency.
    - Added two new endpoints (`/default` and `/default2`) for flexible message handling.
    - Modified `WeatherForecastMessageHandler` for improved logging and handling of `WeatherForecast`.
    - Removed `WeatherForecast2` record to streamline the data model.

--- samples/MessagingSample/Program.cs
+++ samples/MessagingSample/Program.cs
@@ -1,6 +1,7 @@
 using Infinity.Toolkit.Messaging;
 using Infinity.Toolkit.Messaging.Abstractions;
 using Infinity.Toolkit.Messaging.InMemory;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,11 +16,18 @@ builder.AddInfinityMessaging()
                 options.SubscriptionName = "weathersubscription";
             });
 
-        builder
-            .AddChannelProducer<WeatherForecast2>()
-            .AddChannelConsumer<WeatherForecast2>();
+        builder.AddChannelProducer("generic", options => { options.ChannelName = "generic"; });
+        builder.AddChannelConsumer("generic", options =>
+        {
+            options.ChannelName = "generic";
+            options.SubscriptionName = "genericsubscription";
+        });
+
+        builder.AddDefaultChannelProducer();
+        builder.AddDefaultChannelConsumer();
     })
-    .MapMessageHandler<WeatherForecast2, WeatherForecastMessageHandler>();
+    .MapMessageHandler<WeatherForecast, WeatherForecastMessageHandler>()
+    .MapMessageHandler<DefaultMessageHandler>();
 
 
 // Add services to the container.
@@ -55,15 +63,28 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
-app.MapPost("/weatherforecast", async (IChannelProducer<WeatherForecast2> channelProducer) =>
+app.MapPost("/weatherforecast", async ([FromServices]IChannelProducer<WeatherForecast> channelProducer) =>
 {
-    await channelProducer.SendAsync(new WeatherForecast2(DateOnly.FromDateTime(DateTime.Now), 20, "Sunny"), CancellationToken.None);
+    await channelProducer.SendAsync(new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), 20, "Sunny"), CancellationToken.None);
     return Results.Accepted();
 });
 
 app.MapPost("/generic", async ([FromKeyedServices("generic")] IChannelProducer channelProducer) =>
 {
     await channelProducer.SendAsync(new { Message = "Hello, World!" }, CancellationToken.None);
+    return Results.Accepted();
+});
+
+app.MapPost("/default", async (IChannelProducer2 channelProducer) =>
+{
+    await channelProducer.SendAsync<WeatherForecast>(new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), 20, "Cloudy"));
+    return Results.Accepted();
+});
+
+app.MapPost("/default2", async (IChannelProducer2 channelProducer) =>
+{
+    await channelProducer.SendAsync((object)(new { Message = "Hello, World!" }));
+    //await channelProducer.SendAsync(new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), 20, "Rainy"));
     return Results.Accepted();
 });
 
@@ -74,16 +95,22 @@ internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 
-internal record WeatherForecastMessageHandler : IMessageHandler<WeatherForecast2>
+internal record WeatherForecastMessageHandler : IMessageHandler<WeatherForecast>
 {
-    public Task Handle(IMessageHandlerContext<WeatherForecast2> context, CancellationToken cancellationToken)
+    public Task Handle(IMessageHandlerContext<WeatherForecast> context, CancellationToken cancellationToken)
     {
-        Console.WriteLine($"Received message: {context.Message?.Summary} on Channel ${context.ChannelName}");
+        Console.WriteLine($"Received message: {context.Message?.Summary} on Channel {context.ChannelName}");
         return Task.CompletedTask;
     }
 }
 
-internal record WeatherForecast2(DateOnly Date, int TemperatureC, string? Summary)
+public class DefaultMessageHandler(ILogger<DefaultMessageHandler> logger) : IMessageHandler
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    private readonly ILogger<DefaultMessageHandler> logger = logger;
+
+    public Task Handle(IMessageHandlerContext context, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DefaultMessageHandler: Raw message received with sequence number {Message}", context.SequenceNumber);
+        return Task.CompletedTask;
+    }
 }
