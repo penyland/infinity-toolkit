69bc8bf8402784c0f24d0b19087df0167546e5a1
Peter Nylander <peter.nylander@gmail.com>
2025-07-03 17:15:58 +0200

    Add Infinity.Toolkit.EntityFramework project.
    Created `ServiceCollectionExtensions.cs` with a static class that includes a method to remove `DbContext` services from `IServiceCollection`, improving service management flexibility.

---
+++ src/Infinity.Toolkit.EntityFramework/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infinity.Toolkit.EntityFramework;
+
+public static class ServiceCollectionExtensions
+{
+    public static void RemoveDbContext<T>(this IServiceCollection services) where T : DbContext
+    {
+        var descriptor = services.SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<T>));
+        if (descriptor != null)
+        {
+            services.Remove(descriptor);
+        }
+    }
+}
