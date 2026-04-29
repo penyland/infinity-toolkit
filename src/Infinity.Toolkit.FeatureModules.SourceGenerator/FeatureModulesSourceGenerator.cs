c53003b25d46894ff53e3a2d2286d70608d2a6dc
Peter Nylander <peter.nylander@gmail.com>
2026-04-29 14:41:17 +0200

    Add source generator

---
+++ src/Infinity.Toolkit.FeatureModules.SourceGenerator/FeatureModulesSourceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Infinity.Toolkit.FeatureModules.SourceGenerator;
+
+[Generator]
+public class FeatureModulesSourceGenerator : IIncrementalGenerator
+{    
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
